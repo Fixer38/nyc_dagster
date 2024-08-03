de533267860b3070db1edf567473490187836a62
Fixer38 <fixer3838@live.fr>
2024-08-03 09:42:02 +0200

    added weekly partition

--- dagster_university/jobs/__init__.py
+++ dagster_university/jobs/__init__.py
@@ -1,6 +1,6 @@
 from dagster import AssetSelection, define_asset_job
 
-from ..partitions import monthly_partition
+from ..partitions import monthly_partition, weekly_partition
 
 trips_by_week = AssetSelection.assets("trips_by_week")
 trip_update_job = define_asset_job(
@@ -12,4 +12,5 @@ trip_update_job = define_asset_job(
 weekly_update_job = define_asset_job(
     name="weekly_update_job",
     selection=trips_by_week,
+    partitions_def=weekly_partition
 )
