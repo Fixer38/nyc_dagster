dbab0c225da27be7fecf2ee6d8739904fb2a4500
Fixer38 <fixer3838@live.fr>
2024-08-03 09:22:42 +0200

    added parquet files after materializing partitioned asset

--- dagster_university/assets/trips.py
+++ dagster_university/assets/trips.py
@@ -40,35 +40,36 @@ def taxi_zones_file() -> None:
 
 
 @asset(
-    deps=["taxi_trips_file"],
-    partitions_def=monthly_partition
+  deps=["taxi_trips_file"],
+  partitions_def=monthly_partition,
 )
-def taxi_trips(database: DuckDBResource, context: AssetExecutionContext) -> None:
+def taxi_trips(context: AssetExecutionContext, database: DuckDBResource) -> None:
     """
-      The raw taxi trips dataset, loaded into a DuckDB database
+      The raw taxi trips dataset, loaded into a DuckDB database, partitioned by month.
     """
-    partition_key = context.partition_key
-    month_to_fetch = partition_key[:-3]
-    sql_query = f"""
-    /* create initial table with partition_date */
+
+    partition_date_str = context.partition_key
+    month_to_fetch = partition_date_str[:-3]
+
+    query = f"""
       create table if not exists trips (
         vendor_id integer, pickup_zone_id integer, dropoff_zone_id integer,
         rate_code_id double, payment_type integer, dropoff_datetime timestamp,
         pickup_datetime timestamp, trip_distance double, passenger_count double,
         total_amount double, partition_date varchar
       );
-    /* delete any records already inserted as the table will already be created before the first partition materializes */
+
       delete from trips where partition_date = '{month_to_fetch}';
-    /* Insert into table data from raw file + the partition date for tracking records */
+
       insert into trips
-          select
-            VendorID, PULocationID, DOLocationID, RatecodeID, payment_type, tpep_dropoff_datetime,
-            tpep_pickup_datetime, trip_distance, passenger_count, total_amount, '{month_to_fetch}' as partition_date
-          from '{constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)}';
+      select
+        VendorID, PULocationID, DOLocationID, RatecodeID, payment_type, tpep_dropoff_datetime,
+        tpep_pickup_datetime, trip_distance, passenger_count, total_amount, '{month_to_fetch}' as partition_date
+      from '{constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)}';
     """
 
     with database.get_connection() as conn:
-        conn.execute(sql_query)
+        conn.execute(query)
 
 
 @asset(
